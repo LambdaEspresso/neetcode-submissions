e6f609a791072cac89e80de07a06511816b93c02
Lambda Espresso <deekayndkn@gmail.com>
2026-07-21 13:42:16 +0700

    Add: linked-list-cycle-detection - submission-3

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-3.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode node = head;
+        HashSet<Integer> set = new HashSet<>();
+        while (node != null && node.next != null) {
+            if (set.contains(node.next.val)
+             || node.val - node.next.val == 0) return true;
+            else set.add(node.next.val);
+            node = node.next;
+        }
+        return false;
+    }
+}
